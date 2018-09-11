05d0e422dde645c547ca1b4d66efaeef346c8ea6
Karol Klimaszewski <karol.klimaszewski1997@gmail.com>
2018-09-11 11:28:42 +0200

    Added inputs and calculation of BMI

--- src/components/bmi.jsx
+++ src/components/bmi.jsx
@@ -3,12 +3,55 @@ import ReactDOM from 'react-dom';
 export default class Bmi extends React.Component {
    constructor(props){
      super(props);
-
+     this.state = {
+         height: "",
+         weight: ""
+     }
    }
+
+    handleHeightChange = (event) => {
+       if(event.target.value.match(/^[0-9]*$/)){
+           this.setState({
+               height: Number(event.target.value)
+           })
+       }
+    };
+
+    handleWeightChange = (event) => {
+        if(event.target.value.match(/^[0-9]*$/)){
+            this.setState({
+                weight: Number(event.target.value)
+            })
+        }
+    };
+
    render() {
+       //calculating value of BMI
+       let bmiVal = 0;
+       if(this.state.height !== "" && this.state.weight !== ""){
+           bmiVal = (this.state.weight / ((this.state.height / 100) * (this.state.height / 100)));
+       }
+
      return (
          <div className={"container"}>
-             <h1>Hello</h1>
+             <div className="bmi__inputs">
+                 <h2 className={"bmi__title"}>Height:</h2>
+                 <input value={this.state.height} className={"bmi__input"} type="text"
+                 onChange={this.handleHeightChange}/>
+                 <h2 className={"bmi__title"}>Weight:</h2>
+                 <input value={this.state.weight} className={"bmi__input"} type="text"
+                 onChange={this.handleWeightChange}/>
+             </div>
+             <div className="bmi__result">
+             <h3 className={"result__title"}>Your BMI is:</h3>
+             <p className="result__amount">
+                 {bmiVal}
+             </p>
+             <p className="result__category">
+                 everything is ok
+             </p>
+             </div>
+
          </div>
 
      )
